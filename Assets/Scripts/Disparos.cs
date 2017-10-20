12293c28d18cbfe0a16786c3fd47ec3fae7d5691
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2017-10-20 14:59:52 -0500

    Agregar los Empys de los disparos
    Se agregaron 7 Emtys para poder hacer futuras modificaciones

---
+++ Assets/Scripts/Disparos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparos : MonoBehaviour {
+
+	public void Disparar()
+	{
+		if(InputMobileManager.GetTouch ())
+		{
+			
+		}		
+	}
+}
