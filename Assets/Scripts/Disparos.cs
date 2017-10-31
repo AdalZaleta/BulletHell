2c62666e9b2ca4f102f39169dc47d5c209341dbc
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2017-10-31 13:31:41 -0600

    Disparos Basicos
    Ya funcionan los disparos basico ademas de que tiene independiente del movimineto el script de disparo

--- Assets/Scripts/Disparos.cs
+++ Assets/Scripts/Disparos.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class Disparos : MonoBehaviour {
 
-	public void Disparar()
+	public bool puedo = true;
+	public float delay, veldis;
+	public GameObject DisparoIzq, DisparoIzqx, DisparoDer, DisparoDerx ,DisparoCen;
+	public GameObject bala;
+	public int Modo;
+
+	void Update ()
 	{
-		if(InputMobileManager.GetTouch ())
+		if (InputMobileManager.GetTouch () && puedo && Modo == 1)
 		{
-			
-		}		
+			GameObject go = Instantiate (bala, DisparoIzq.gameObject.transform.position, Quaternion.identity);
+			GameObject go1 = Instantiate (bala, DisparoDer.gameObject.transform.position, Quaternion.identity);
+			go.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * veldis);
+			go1.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * veldis);
+			puedo = false;
+			StartCoroutine (Disparar ());
+		}
+	}
+
+	IEnumerator Disparar()
+	{
+		yield return new WaitForSeconds (delay);
+		puedo = true;
 	}
 }
