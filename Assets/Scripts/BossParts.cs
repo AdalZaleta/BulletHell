11d3e871e6b36ba171033ffd4562ee8fa67c2a25
Ngakk <ngakwastaken@gmail.com>
2017-11-08 12:53:34 -0600

    Añadi valor de daño para las balas

---
+++ Assets/Scripts/BossParts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossParts : MonoBehaviour {
+
+	public float HP;
+	public float Armor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D _col)
+	{
+		if (_col.CompareTag ("Bullet")) {
+			
+		}
+	}
+}
