bab34c5951b89f2dd95669542d70b644c7a2c830
Retus001 <retus001@gmail.com>
2017-11-10 00:38:08 -0600

    Update Boss

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -140,7 +140,7 @@ public class Shoot : MonoBehaviour {
 
 	IEnumerator Disparar()
 	{
-		yield return new WaitForSeconds (Time.deltaTime * delay);
+		yield return new WaitForSeconds (delay);
 		canshoot = true;
 	}
 }
