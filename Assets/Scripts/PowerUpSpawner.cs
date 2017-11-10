18a88c35fdaf499ea3e65d99a02208e45689cfb9
Retus001 <retus001@gmail.com>
2017-11-10 13:18:36 -0600

    Powerup spawner

---
+++ Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour {
+
+	public GameObject PU_D;
+	public GameObject PU_L;
+	public GameObject PU_S;
+	public GameObject PU_Q;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
